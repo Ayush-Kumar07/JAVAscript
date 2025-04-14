51f871b184d0829a33dc40f279a02c94fc3ec3d7
Ayush Kumar <152910752+Ayush-Kumar07@users.noreply.github.com>
2025-04-14 22:35:29 +0530

    Add files via upload

---
+++ firstLec.js
@@ -0,0 +1,32 @@
+// console.log("Welocome to java script ");
+// fullname="Ayush Yadav";
+// console.log(fullname)
+
+
+
+
+// *******Product information recereate 
+
+
+// const product = {
+//     title: "PARKER ball pen",
+//     rating: 4,
+//     offer: 5,
+//     price: 300,
+//   };
+  
+//   console.log(product);
+  
+
+
+// ******Instagram Profile 
+
+// 
+const profile={
+    username: "@ayusx_h",
+    isfollow: false,
+    followers: 556,
+    following: 684,
+
+}
+console.log(profile);
